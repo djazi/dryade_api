3809e74c0b6830c9292d1a2f32499360faf74891
@k.djazi <k.djazi@tnaconsulting.net>
2022-09-08 17:21:29 +0100

    add nested serializer for order and users

--- dryade_api/orders/api/serializers.py
+++ dryade_api/orders/api/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
 from dryade_api.orders.models import Order
+from dryade_api.users.models import User
+
+
+class UserOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "is_staff", "is_active"]
 
 
 class OrderSerializer(serializers.ModelSerializer):
+    user = UserOrderSerializer(read_only=True)
+
     class Meta:
         model = Order
         fields = ["order_name", "timestamp", "completed", "updated", "user"]
