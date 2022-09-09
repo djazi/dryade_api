ef99f1a5ef28d329462a8600cb7ef45a9f5bfec1
@k.djazi <k.djazi@tnaconsulting.net>
2022-09-09 11:41:15 +0100

    separate the crud in different apis
    soft delete for orders
    
    display fields for order in admin

--- dryade_api/orders/admin.py
+++ dryade_api/orders/admin.py
@@ -2,4 +2,17 @@ from django.contrib import admin
 
 from dryade_api.orders.models import Order
 
-admin.site.register(Order)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "order_name",
+        "created_by",
+        "completed",
+        "created_at",
+        "updated_at",
+        "is_deleted",
+    )
+
+
+admin.site.register(Order, OrderAdmin)
--- dryade_api/orders/models.py
+++ dryade_api/orders/models.py
@@ -1,14 +1,21 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
-from dryade_api.users.models import User
+from dryade_api.users.models import BaseModel, User
 
 
-class Order(models.Model):
+class Order(BaseModel):
     order_name = models.CharField(max_length=180)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False, blank=True)
     completed = models.BooleanField(default=False, blank=True)
-    updated = models.DateTimeField(auto_now=True, blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True
+    )
+
+    def clean(self):
+        """-- call it by 'obj.full_clean() --"""
+        if self.order_name == self.created_by:
+            raise ValidationError("The order name cannot be the same as the username ")
 
     def __str__(self):
         return self.order_name
